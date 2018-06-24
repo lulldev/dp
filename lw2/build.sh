a0f41c7d1363009183290b3260fdf3bdb2335469
lulldev <lulldev@yandex.ru>
2018-06-24 22:23:07 +0300

    lw2
    ready lw2 configuration

--- lw2/build.sh
+++ lw2/build.sh
@@ -7,12 +7,12 @@ fi
 
 scriptPath="$( cd "$(dirname "$0")" ; pwd -P)"
 
-mkdir -p $scriptPath/build/$1/config
+mkdir -p $scriptPath/$1/config
 
 cd $scriptPath/src/Frontend
-dotnet publish --configuration Release --framework netcoreapp2.0 -o $scriptPath/build/$1/Frontend /property:PublishWithAspNetCoreTargetManifest=false
+dotnet publish --configuration Release --framework netcoreapp2.0 -o $scriptPath/$1/Frontend /property:PublishWithAspNetCoreTargetManifest=false
 cd $scriptPath/src/Backend
-dotnet publish --configuration Release --framework netcoreapp2.0 -o $scriptPath/build/$1/Backend /property:PublishWithAspNetCoreTargetManifest=false
+dotnet publish --configuration Release --framework netcoreapp2.0 -o $scriptPath/$1/Backend /property:PublishWithAspNetCoreTargetManifest=false
 
-cp $scriptPath/src/run.sh $scriptPath/build/$1
-cp $scriptPath/src/stop.sh $scriptPath/build/$1
+cp $scriptPath/src/run.sh $scriptPath/$1
+cp $scriptPath/src/stop.sh $scriptPath/$1
--- lw2/src/run.sh
+++ lw2/src/run.sh
@@ -2,8 +2,8 @@
 
 frontendPID=-1
 backendPID=-1
-exec dotnet Frontend/Frontend.dll --configuration Release --launch-profile Production & frontendPID=$!
-exec dotnet Backend/Backend.dll --configuration Release --launch-profile Production & backendPID=$!
+dotnet Frontend/Frontend.dll --configuration Release --launch-profile Production & frontendPID=$!
+dotnet Backend/Backend.dll --configuration Release --launch-profile Production & backendPID=$!
 
 echo $frontendPID >> "pid"
 echo $backendPID >> "pid"
