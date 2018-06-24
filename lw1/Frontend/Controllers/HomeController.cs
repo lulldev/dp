a87b7d281059ffa76af5cedc0e9afbbcb3a1b047
lulldev <lulldev@yandex.ru>
2018-06-24 19:53:59 +0300

    init lw1

---
+++ lw1/Frontend/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Frontend.Models;
+using System.Net.Http;
+
+namespace Frontend.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static readonly HttpClient httpClient = new HttpClient();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload(string data)
+        {
+            var dictionary = new Dictionary<string, string>()
+            {
+                { "data", (data == null) ? "" : data },
+            };
+            var content = new FormUrlEncodedContent(dictionary);
+            var response = await httpClient.PostAsync("http://127.0.0.1:5000/api/values", content);
+            var result = await response.Content.ReadAsStringAsync();
+            return Ok(result);
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
